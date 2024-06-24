802fad9c5f42661fc6dac55746b4608b9553b2c6
lahiruvimukthi292 <lahiruvimukthi292@gmail.com>
2024-06-24 02:32:08 +0000

    add

--- src/Pages/Main/Chat.jsx
+++ src/Pages/Main/Chat.jsx
@@ -1,6 +1,4 @@
-import { Grid } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import SideBar from '../../Components/SideBar/SideBar';
 import Navbar from '../../Components/NavBar/Navbar';
 
 const defaultTheme = createTheme();
@@ -10,9 +8,7 @@ export default function Chat () {
 
     return (
         <ThemeProvider theme={defaultTheme}>
-             <Navbar />             
-            <Grid container component="main" sx={{ height: '100vh',width:'300%' }}>                
-            </Grid>            
+             <Navbar />                               
         </ThemeProvider>
     );
 }
